89f0d23ccb44e8d2294b1b0b80b7806753193954
Tobias Johansson <tobiyanen@gmail.com>
2020-03-11 18:11:19 +0100

    Uppdatering på kurslänkar och menylänkar

---
+++ js/error.js
@@ -0,0 +1,6 @@
+$("document").ready(function () {
+  $("button").click(function () {
+    window.location.href = "login.html";
+  })
+  setTimeout(function () { window.location.href = "login.html"; }, 5000);
+});
--- js/script.js
+++ js/script.js
@@ -1,85 +1,30 @@
 $("document").ready(function () {
 
-  function loginScript() {
-    //Start to get data from API about students and login information
-    $.get("http://webbred2.utb.hb.se/~fewe/api/api.php?data=students", function (
-      data
-    ) {
-      $("#submit-button").click(function () {
-        // Get the value from the input fields
-        var username = $("#username").val();
-        var password = $("#password").val();
-        var logged_in = false;
-
-        // For loop to check if the input value is same as API data
-        if (username == "" || password == "") {
-          $("#login-box")
-            .append(
-              "<p class='wrong-login'>Alla fält måste vara ifyllda</p>"
-            )
-            .find(".wrong-login") // Find the wrong-login class text
-            .fadeOut(5000); // Add awesome fade effect
-        } else {
-          login()
-        }
-
-        function login() {
-          var loginSuccessful = 0;
-          for (i = 0; i < data.length; i++) {
-            if (
-              username == data[i].login.username &&
-              password == data[i].login.password
-            ) {
-              loginSuccessful++; // Add value to variable if successful
-              sessionStorage.setItem('username', username)
-            }
-          }
-          if (loginSuccessful == 0) {
-            // If unsuccessful the loginSuccessful variable is 0, then run the follwing
-            $(".wrong-login").remove(); // Needs this to remove the text to see if next input is invalid as well.
-            $("#login-box")
-              .append(
-                "<p class='wrong-login'>Du har skrivit fel användarnamn<br>och / eller lösenord</p>"
-              )
-              .find(".wrong-login") // Find the wrong-login class text
-              .fadeOut(5000); // Add awesome fade effect
-          } else if (loginSuccessful == 1) {
-            logged_in = true;
-            // If loginSuccessful has 1 value, redirect to next page.
-            window.location.href = "courseinfo.html";
-          }
-        }
-      });
-    });
-    //End API get about student logins
+  function checkSession() {
+    if (sessionStorage.getItem('username') == null) {
+      window.location.href = "error.html";
+    }
   }
 
-  function courses() {
-    //Start to get data from API about course information
-    $.get("http://webbred2.utb.hb.se/~fewe/api/api.php?data=courses", function (
-      courseData
-    ) {
-      // Check if the session is valid, or else
-      //   function checkSession() {
-      //     if (sessionStorage.getItem('username') == null) {
-      //       window.location.href = "error.html";
-      //     }
-      //   }
-      //
-      //
-      //   function checkUser() {
-      //     if (sessionStorage.getItem('username') != null) {
-      //       $(".user").append("Inloggad som " + sessionStorage.getItem('username'));
-      //     } else {
-      //       checkSession();
-      //     }
-      //   }
-      //   checkUser();
-      // For loop to append API course data to the <th> in html
+  function checkUser() {
+    if (sessionStorage.getItem('username') != null) {
+      $(".user").append("Inloggad som " + sessionStorage.getItem('username'));
+    } else {
+      checkSession();
+    }
+  }
+  checkUser();
+  //For loop to append API course data to the <th> in html
 
+
+  function addLogOutButton() {
+    $(".navbar-nav").append("<li class='nav-item'><button class='btn btn-primary justify-content-start btn-logout'>Logga ut</button></li>");
+    $(".btn-logout").click(function () {
+      sessionStorage.clear();
+      window.location.href="login.html"
     });
   }
-  // courses();
+  addLogOutButton();
 
   function addCourses() {
     $.get("http://webbred2.utb.hb.se/~fewe/api/api.php?data=courses", courseData => {
@@ -90,8 +35,10 @@ $("document").ready(function () {
         $(".menu-item").eq(index).attr("href", coursesLink + ".html").html(courses.courseName);
 
         $("tbody").append(
-          "<tr><td><a class='course-link' href='" + coursesLink + ".html'>" + courses.school + "</a></td><td><a class='course-link' href='" + coursesLink + ".html'>" + courses.courseId + "</a></td><td><a class='course-link' href='" + coursesLink + ".html'>" + courses.courseName + "</a></td><td><a class='course-link' href='" + coursesLink + ".html'>" + courses.credit + "</a></td><td><a class='course-link' href='" + coursesLink + ".html'>" + courses.startWeek + "</a></td><td><a class='course-link' href='" + coursesLink + ".html'>" + courses.endWeek + "</a></td></tr>"
+          "<tr><td><a class='course-link course-" + index + "' href='" + coursesLink + ".html'>" + courses.school + "</a></td><td><a class='course-link course-" + index + "' href='" + coursesLink + ".html'>" + courses.courseId + "</a></td><td><a class='course-link course-" + index + "' href='" + coursesLink + ".html'>" + courses.courseName + "</a></td><td><a class='course-link course-" + index + "' href='" + coursesLink + ".html'>" + courses.credit + "</a></td><td><a class='course-link course-" + index + "' href='" + coursesLink + ".html'>" + courses.startWeek + "</a></td><td><a class='course-link course-" + index + "' href='" + coursesLink + ".html'>" + courses.endWeek + "</a></td></tr>"
         );
+
+        
 
         $(".start-h" + index).append("<h5 class='card-title d-inline'>Skola </h5><p class='card-text d-inline font-weight-bold'> " + courses.school + "</p><br>")
         $(".start-h" + index).append("<h5 class='card-title d-inline'>Poäng </h5><p class='card-text d-inline font-weight-bold'> " + courses.credit + "</p><br>")
@@ -99,11 +46,17 @@ $("document").ready(function () {
         $(".start-h" + index).append("<h5 class='card-title d-inline'>Slutar vecka </h5><p class='card-text d-inline font-weight-bold'> " + courses.endWeek + "</p><br>")
         $(".start-h" + index).append("<h5 class='card-title d-inline'>Lärare </h5><p class='card-text d-inline font-weight-bold'> " + courses.teachers + "</p><br>")
 
+        $(".course-2").removeAttr("href");
+        $(".course-4").removeAttr("href");
+        $(".course-5").removeAttr("href");
+        $(".course-6").removeAttr("href");
       });
 
     });
   }
   addCourses();
+
+
 
   function addQuiz() {
     $.get("http://webbred2.utb.hb.se/~fewe/api/api.php?data=quiz", quizData => {
@@ -113,7 +66,7 @@ $("document").ready(function () {
 
         $(".quizdiv").append(
           $(
-            "<h3 class='question-box mt-5'>" + quiz.question + "</h3>"
+            "<h4 class='question-box mt-5'>" + quiz.question + "</h4>"
           )
         );
 
